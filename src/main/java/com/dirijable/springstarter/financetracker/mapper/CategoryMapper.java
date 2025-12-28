3ab7c2262f8082f1fa248e97841e14b793fade38
Dirijable <checkpuk1379@gmail.com>
2025-12-28 22:35:30 +0300

    refactor: migrate User and Category mappers to MapStruct

--- src/main/java/com/dirijable/springstarter/financetracker/mapper/CategoryMapper.java
+++ src/main/java/com/dirijable/springstarter/financetracker/mapper/CategoryMapper.java
@@ -4,29 +4,44 @@ import com.dirijable.springstarter.financetracker.database.entity.Category;
 import com.dirijable.springstarter.financetracker.dto.category.CategoryCreateDto;
 import com.dirijable.springstarter.financetracker.dto.category.CategoryResponseDto;
 import com.dirijable.springstarter.financetracker.dto.category.CategoryUpdateDto;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
 
-@Component
-public class CategoryMapper {
-    public Category toEntity(CategoryCreateDto createDto){
-        return Category.builder()
-                .name(createDto.name())
-                .description(createDto.description())
-                .build();
-    }
+@Mapper(
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public interface CategoryMapper {
 
-    public void updateEntity(CategoryUpdateDto updateDto, Category category){
-        if(updateDto.name() != null)
-            category.setName(updateDto.name());
-        if(updateDto.description() != null)
-            category.setDescription(updateDto.description());
-    }
+    Category toEntity(CategoryCreateDto createDto);
 
-    public CategoryResponseDto toResponse(Category category){
-        return new CategoryResponseDto(
-                category.getId(),
-                category.getName(),
-                category.getDescription()
-        );
-    }
+    void updateEntity(CategoryUpdateDto updateDto, @MappingTarget Category category);
+
+    CategoryResponseDto toResponse(Category category);
 }
+
+//
+//public Category toEntity(CategoryCreateDto createDto){
+//    return Category.builder()
+//            .name(createDto.name())
+//            .description(createDto.description())
+//            .build();
+//}
+//
+//public void updateEntity(CategoryUpdateDto updateDto, Category category){
+//    if(updateDto.name() != null)
+//        category.setName(updateDto.name());
+//    if(updateDto.description() != null)
+//        category.setDescription(updateDto.description());
+//}
+//
+//public CategoryResponseDto toResponse(Category category){
+//    return new CategoryResponseDto(
+//            category.getId(),
+//            category.getName(),
+//            category.getDescription()
+//    );
+//}
--- src/main/java/com/dirijable/springstarter/financetracker/mapper/UserMapper.java
+++ src/main/java/com/dirijable/springstarter/financetracker/mapper/UserMapper.java
@@ -4,32 +4,48 @@ import com.dirijable.springstarter.financetracker.database.entity.User;
 import com.dirijable.springstarter.financetracker.dto.user.UserCreateDto;
 import com.dirijable.springstarter.financetracker.dto.user.UserResponseDto;
 import com.dirijable.springstarter.financetracker.dto.user.UserUpdateDto;
+import org.mapstruct.*;
 import org.springframework.stereotype.Component;
 
-@Component
-public class UserMapper {
-    public User toEntity(UserCreateDto createDto) {
-        return User.builder()
-                .email(createDto.email())
-                .password(createDto.password())
-                .username(createDto.username())
-                .build();
-    }
+@Mapper(
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public interface UserMapper {
 
-    public void updateEntity(UserUpdateDto updateDto, User user) {
-        if (updateDto.email() != null)
-            user.setEmail(updateDto.email());
-        if (updateDto.username() != null)
-            user.setUsername(updateDto.username());
-        if (updateDto.password() != null)
-            user.setPassword(updateDto.password());
-    }
+    User toEntity(UserCreateDto createDto);
 
-    public UserResponseDto toResponse(User user) {
-        return new UserResponseDto(
-                user.getId(),
-                user.getEmail(),
-                user.getUsername()
-        );
-    }
+    void updateEntity(UserUpdateDto updateDto, @MappingTarget User user);
+
+    UserResponseDto toResponse(User user);
 }
+
+
+
+
+//
+//public User toEntity(UserCreateDto createDto) {
+//    return User.builder()
+//            .email(createDto.email())
+//            .password(createDto.password())
+//            .username(createDto.username())
+//            .build();
+//}
+//
+//public void updateEntity(UserUpdateDto updateDto, User user) {
+//    if (updateDto.email() != null)
+//        user.setEmail(updateDto.email());
+//    if (updateDto.username() != null)
+//        user.setUsername(updateDto.username());
+//    if (updateDto.password() != null)
+//        user.setPassword(updateDto.password());
+//}
+//
+//public UserResponseDto toResponse(User user) {
+//    return new UserResponseDto(
+//            user.getId(),
+//            user.getEmail(),
+//            user.getUsername()
+//    );
+//}
