0779bc3a4d8d87d44ae5ed710a04a7fee24de0f1
Dirijable <checkpuk1379@gmail.com>
2025-12-31 00:41:58 +0300

    add account response dto for future account service implementaion

--- src/main/java/com/dirijable/springstarter/financetracker/dto/account/AccountResponseDto.java
+++ src/main/java/com/dirijable/springstarter/financetracker/dto/account/AccountResponseDto.java
@@ -1,4 +1,13 @@
 package com.dirijable.springstarter.financetracker.dto.account;
 
-public record AccountResponseDto() {
+import com.dirijable.springstarter.financetracker.database.entity.Currency;
+
+import java.math.BigDecimal;
+
+public record AccountResponseDto(
+        Long id,
+        String name,
+        BigDecimal balance,
+        Currency currency
+) {
 }
