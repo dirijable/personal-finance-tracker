d1b1a5a16c981a99accb0a12663d8e2f8cb2a0e2
Dirijable <checkpuk1379@gmail.com>
2025-12-30 23:58:03 +0300

    feat: add account DTOs for future account service implementation

--- src/main/java/com/dirijable/springstarter/financetracker/dto/account/AccountCreateDto.java
+++ src/main/java/com/dirijable/springstarter/financetracker/dto/account/AccountCreateDto.java
@@ -1,4 +1,13 @@
 package com.dirijable.springstarter.financetracker.dto.account;
 
-public class AccountCreateDto {
-}
+import com.dirijable.springstarter.financetracker.database.entity.Currency;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+import java.math.BigDecimal;
+
+public record AccountCreateDto(
+        @NotBlank String name,
+        @NotBlank @Min(value = 0) BigDecimal balance,
+        @NotBlank Currency currency
+) {}
--- src/main/java/com/dirijable/springstarter/financetracker/dto/account/AccountUpdateDto.java
+++ src/main/java/com/dirijable/springstarter/financetracker/dto/account/AccountUpdateDto.java
@@ -1,4 +1,13 @@
 package com.dirijable.springstarter.financetracker.dto.account;
 
-public class AccountUpdateDto {
+import com.dirijable.springstarter.financetracker.database.entity.Currency;
+import jakarta.validation.constraints.Min;
+
+import java.math.BigDecimal;
+
+public record AccountUpdateDto(
+        String name,
+        @Min(value = 0) BigDecimal balance,
+        Currency currency
+) {
 }
