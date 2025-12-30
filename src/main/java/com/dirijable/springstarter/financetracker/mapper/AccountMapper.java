6ca0b08fcbce03990414ac38a473221f6da45d81
Dirijable <checkpuk1379@gmail.com>
2025-12-30 23:58:35 +0300

    add account mapper

--- src/main/java/com/dirijable/springstarter/financetracker/mapper/AccountMapper.java
+++ src/main/java/com/dirijable/springstarter/financetracker/mapper/AccountMapper.java
@@ -1,4 +1,17 @@
 package com.dirijable.springstarter.financetracker.mapper;
 
+import com.dirijable.springstarter.financetracker.database.entity.Account;
+import com.dirijable.springstarter.financetracker.dto.account.AccountCreateDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface AccountMapper {
+
+    public Account toEntity(AccountCreateDto createDto);
 }
