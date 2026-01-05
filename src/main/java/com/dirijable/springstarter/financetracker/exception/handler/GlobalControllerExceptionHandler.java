e888502d3305b64f34b18203e9d6910311f56bf0
Dirijable <checkpuk1379@gmail.com>
2026-01-05 21:30:03 +0300

    feat: add global exception handler for rest controllers

--- src/main/java/com/dirijable/springstarter/financetracker/exception/handler/GlobalControllerExceptionHandler.java
+++ src/main/java/com/dirijable/springstarter/financetracker/exception/handler/GlobalControllerExceptionHandler.java
@@ -1,14 +1,57 @@
-package com.dirijable.springstarter.financetracker.hadler;
+package com.dirijable.springstarter.financetracker.exception.handler;
 
+import com.dirijable.springstarter.financetracker.exception.base.FinanceTrackerException;
+import com.dirijable.springstarter.financetracker.exception.dto.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice(basePackages = "com.dirijable.springstarter.financetracker.http.rest")
 @Slf4j
 public class GlobalControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
-    public void a(){
-        ErrorRes
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().stream().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        ErrorResponse errorResponse = new ErrorResponse(
+                status.value(),
+                "Validation failed",
+                LocalDateTime.now(),
+                errors
+        );
+        return ResponseEntity.badRequest().body(errorResponse);
     }
+
+
+    @ExceptionHandler(FinanceTrackerException.class)
+    public ResponseEntity<ErrorResponse> handleFinanceTrackerException(FinanceTrackerException ex) {
+        ErrorResponse error = new ErrorResponse(
+                ex.getStatus().value(),
+                ex.getMessage(),
+                LocalDateTime.now(),
+                null
+        );
+        return ResponseEntity
+                .status(ex.getStatus())
+                .body(error);
+    }
+
 }
