010010a895cee1f2a77648644e86c5db65f93f2e
Dirijable <checkpuk1379@gmail.com>
2026-01-05 21:29:23 +0300

    feat: add user and category rest contollers

--- src/main/java/com/dirijable/springstarter/financetracker/http/rest/CategoryRestController.java
+++ src/main/java/com/dirijable/springstarter/financetracker/http/rest/CategoryRestController.java
@@ -1,4 +1,60 @@
 package com.dirijable.springstarter.financetracker.http.rest;
 
+import com.dirijable.springstarter.financetracker.dto.category.CategoryCreateDto;
+import com.dirijable.springstarter.financetracker.dto.category.CategoryResponseDto;
+import com.dirijable.springstarter.financetracker.dto.category.CategoryUpdateDto;
+import com.dirijable.springstarter.financetracker.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users/{userId}/categories")
+@RequiredArgsConstructor
 public class CategoryRestController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponseDto>> findAllByUserId(@PathVariable("userId") Long userId) {
+        return ResponseEntity.ok(categoryService.findAllByUserId(userId));
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<CategoryResponseDto> findById(@PathVariable("categoryId") Long categoryId,
+                                                        @PathVariable("userId") Long userId) {
+        return ResponseEntity.ok(categoryService.findById(categoryId, userId));
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryResponseDto> create(@PathVariable("userId") Long userId,
+                                                      @RequestBody @Validated CategoryCreateDto createDto) {
+        CategoryResponseDto response = categoryService.add(createDto, userId);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+        return ResponseEntity.created(uri)
+                .body(response);
+    }
+
+    @PatchMapping("/{categoryId}")
+    public ResponseEntity<CategoryResponseDto> updateById(@PathVariable("categoryId") Long categoryId,
+                                                          @PathVariable("userId") Long userId,
+                                                          @RequestBody @Validated CategoryUpdateDto updateDto) {
+        CategoryResponseDto responseDto = categoryService.updateById(updateDto, categoryId, userId);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<Void> deleteById(@PathVariable("categoryId") Long categoryId,
+                           @PathVariable("userId") Long userId) {
+        categoryService.deleteCategory(categoryId, userId);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/dirijable/springstarter/financetracker/http/rest/UserRestController.java
+++ src/main/java/com/dirijable/springstarter/financetracker/http/rest/UserRestController.java
@@ -1,4 +1,57 @@
 package com.dirijable.springstarter.financetracker.http.rest;
 
+import com.dirijable.springstarter.financetracker.dto.user.UserCreateDto;
+import com.dirijable.springstarter.financetracker.dto.user.UserResponseDto;
+import com.dirijable.springstarter.financetracker.dto.user.UserUpdateDto;
+import com.dirijable.springstarter.financetracker.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserRestController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDto>> findAll() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponseDto> findById(@PathVariable("userId") Long userId) {
+        return ResponseEntity.ok(userService.findById(userId));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> create(@RequestBody @Validated UserCreateDto createDto) {
+
+        UserResponseDto responseDto = userService.create(createDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(responseDto.id())
+                .toUri();
+        return ResponseEntity.created(uri)
+                .body(responseDto);
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<UserResponseDto> update(@PathVariable("userId") Long userId,
+                                                  @RequestBody @Validated UserUpdateDto updateDto) {
+        return ResponseEntity.ok(userService.updateById(userId, updateDto));
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> delete(@PathVariable("userId") Long userId) {
+        userService.deleteById(userId);
+        return ResponseEntity.noContent().build();
+    }
+
 }
